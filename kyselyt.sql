22a065758b463332f10a3459c83988634f55e3bf
johannes <johannes.jokinen@metropolia.fi>
2021-11-30 16:08:31 +0200

    4

--- kyselyt.sql
+++
@@ -1,6 +0,0 @@
-USE isofirma;
-SELECT etunimi, palkka FROM tyontekija WHERE sukunimi = 'Hämes';
-SELECT etunimi, palkka FROM tyontekija WHERE sukunimi = 'Marttinen';
-SELECT etunimi, palkka FROM tyontekija WHERE sukunimi = 'Kasari';
-SELECT etunimi, palkka FROM tyontekija WHERE sukunimi = 'Kivisaari';
-SELECT etunimi, palkka FROM tyontekija WHERE sukunimi = 'Wuolab';
